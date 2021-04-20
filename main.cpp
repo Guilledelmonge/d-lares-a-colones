ccc3cb58512b491278b5e2089c2c73e47bd096af
Guillermo Delgado <>
2021-04-20 15:27:43 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int dolares;
+  int altura;
+  float colones;
+
+std::cout << "digite la cantidad de dolares: " << std ::endl;
+std::cin  >> dolares;
+
+colones= (dolares*615.98);
+
+std::cout << "el monto es : " <<colones << " colones"<< std ::endl;
+}
